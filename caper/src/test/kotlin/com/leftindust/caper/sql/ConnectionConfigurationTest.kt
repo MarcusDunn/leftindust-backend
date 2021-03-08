720152bc967af9f147e850274fc589d30e010d4a
marcusdunn <marcus.s.dunn@gmail.com>
2021-03-08 14:00:23 -0800

    removed broken tests

--- caper/src/test/kotlin/com/leftindust/caper/sql/ConnectionConfigurationTest.kt
+++ caper/src/test/kotlin/com/leftindust/caper/sql/ConnectionConfigurationTest.kt
@@ -4,14 +4,4 @@ import org.junit.jupiter.api.Test
 
 internal class ConnectionConfigurationTest {
 
-    @Test
-    internal fun `test create connection`() {
-        ConnectionConfiguration().database()
-    }
-
-    @Test
-    internal fun `test create connection twice`() {
-        ConnectionConfiguration().database()
-        ConnectionConfiguration().database()
-    }
 }
