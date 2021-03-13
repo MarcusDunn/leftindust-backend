22dfd1d849dc99161c9df805dbf00e898a61a38b
marcusdunn <marcus.s.dunn@gmail.com>
2021-03-12 22:04:01 -0800

    added first test

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/auth/impl/AuthorizerImpl.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/auth/impl/AuthorizerImpl.kt
@@ -3,9 +3,9 @@ package com.leftindust.mockingbird.auth.impl
 import com.leftindust.mockingbird.auth.Authorizer
 import com.leftindust.mockingbird.auth.MediqToken
 import com.leftindust.mockingbird.dao.AuthorizationDao
+import com.leftindust.mockingbird.dao.entity.AccessControlList
 import com.leftindust.mockingbird.dao.entity.Action
 import com.leftindust.mockingbird.extensions.Authorization
-import org.apache.logging.log4j.LogManager
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
@@ -18,12 +18,18 @@ internal class AuthorizerImpl(
     @Autowired private val authorizationDao: AuthorizationDao
 ) : Authorizer {
     override suspend fun getAuthorization(action: Action, user: MediqToken): Authorization {
-        return (authorizationDao
-            .getRolesForUserByUid(user.uid ?: return Authorization.Denied)
-            .getOrNull() ?: return Authorization.Denied)
+        return (getRoles(user) ?: return Authorization.Denied)
             .map { it.action }
             .contains(action)
             .toAuthorization()
+    }
+
+    private suspend fun getRoles(user: MediqToken): List<AccessControlList>? {
+        return user.uid?.let { uid ->
+            authorizationDao
+                .getRolesForUserByUid(uid)
+                .getOrNull()
+        }
     }
 
     private fun Boolean.toAuthorization() = if (this) Authorization.Allowed else Authorization.Denied
--- mockingbird/src/test/kotlin/com/leftindust/mockingbird/auth/impl/AuthorizerImplTest.kt
+++ mockingbird/src/test/kotlin/com/leftindust/mockingbird/auth/impl/AuthorizerImplTest.kt
@@ -1,13 +1,49 @@
 package com.leftindust.mockingbird.auth.impl
 
+import com.leftindust.mockingbird.auth.Crud.READ
+import com.leftindust.mockingbird.dao.AuthorizationDao
+import com.leftindust.mockingbird.dao.Tables.Patient
+import com.leftindust.mockingbird.dao.entity.Action
+import com.leftindust.mockingbird.extensions.Authorization
+import com.leftindust.mockingbird.extensions.Success
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 
-import org.junit.jupiter.api.Assertions.*
-
+@ExtendWith(MockKExtension::class)
 internal class AuthorizerImplTest {
 
+    @MockK
+    private lateinit var authorizationDao: AuthorizationDao
 
     @Test
     fun getAuthorization() {
+        val readToPatients = mockk<Action> {
+            every { referencedTableName } returns Patient
+            every { permissionType } returns READ
+        }
+
+        coEvery { authorizationDao.getRolesForUserByUid("marcus") } returns Success(listOf(
+            mockk {
+                every { readToPatients } returns readToPatients
+            }
+        ))
+
+        val actual = runBlocking {
+            AuthorizerImpl(authorizationDao).getAuthorization(readToPatients, mockk {
+                every { uid } returns "marcus"
+            })
+        }
+
+        assertEquals(Authorization.Allowed, actual)
+
+        coVerify { authorizationDao.getRolesForUserByUid("marcus") }
     }
 }
