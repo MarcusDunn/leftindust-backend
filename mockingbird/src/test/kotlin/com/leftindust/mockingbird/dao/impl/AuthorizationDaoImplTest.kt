6766329887d59fb0874736ab218de6afb13d3f38
marcusdunn <marcus.s.dunn@gmail.com>
2021-03-13 17:07:57 -0800

    added more testing

---
+++ mockingbird/src/test/kotlin/com/leftindust/mockingbird/dao/impl/AuthorizationDaoImplTest.kt
@@ -0,0 +1,33 @@
+package com.leftindust.mockingbird.dao.impl
+
+import com.leftindust.mockingbird.dao.entity.AccessControlList
+import com.leftindust.mockingbird.dao.entity.MediqUser
+import com.leftindust.mockingbird.dao.impl.repository.HibernateAclRepository
+import com.leftindust.mockingbird.dao.impl.repository.HibernateUserRepository
+import com.leftindust.mockingbird.extensions.Success
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class AuthorizationDaoImplTest {
+    private val aclRepository = mockk<HibernateAclRepository>()
+    private val userRepository = mockk<HibernateUserRepository>()
+
+    @Test
+    fun getRolesForUserByUid() {
+        val acl = mockk<AccessControlList>()
+
+        val mediqUser = mockk<MediqUser>("marcus") {
+            every { group } returns null
+        }
+
+        every { userRepository.getUserByUniqueId("uid") } returns mediqUser
+        every { aclRepository.findAllByMediqUser(mediqUser) } returns listOf(acl)
+
+        val authorizationDaoImpl = AuthorizationDaoImpl(aclRepository, userRepository)
+        val result = runBlocking { authorizationDaoImpl.getRolesForUserByUid("uid") }
+        assertEquals(Success(listOf(acl)), result)
+    }
+}
