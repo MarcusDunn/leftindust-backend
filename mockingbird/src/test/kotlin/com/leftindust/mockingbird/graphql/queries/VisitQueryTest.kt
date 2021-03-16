f0fb74334c31d82a41bd1068b8a302070eaff8cd
marcusdunn <marcus.s.dunn@gmail.com>
2021-03-15 21:31:20 -0700

    visit test

---
+++ mockingbird/src/test/kotlin/com/leftindust/mockingbird/graphql/queries/VisitQueryTest.kt
@@ -0,0 +1,32 @@
+package com.leftindust.mockingbird.graphql.queries
+
+import com.leftindust.mockingbird.auth.GraphQLAuthContext
+import com.leftindust.mockingbird.dao.VisitDao
+import com.leftindust.mockingbird.dao.entity.Visit
+import com.leftindust.mockingbird.extensions.Success
+import com.leftindust.mockingbird.extensions.gqlID
+import com.leftindust.mockingbird.graphql.types.GraphQLVisit
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class VisitQueryTest {
+    private val visitDao = mockk<VisitDao>()
+    private val graphQLAuthContext = mockk<GraphQLAuthContext>()
+
+    @Test
+    fun getVisitsByPatient() {
+        every {graphQLAuthContext.mediqAuthToken} returns mockk()
+        val mockkVisit = mockk<Visit>(relaxed = true) {
+            every { id } returns 2000
+        }
+        coEvery { visitDao.getVisitsForPatientPid(1000, any()) } returns Success(listOf(mockkVisit))
+        val visitQuery = VisitQuery(visitDao)
+        val result = runBlocking { visitQuery.getVisitsByPatient(gqlID(1000), graphQLAuthContext) }
+
+        assertEquals(listOf(GraphQLVisit(mockkVisit, mockkVisit.id!!, graphQLAuthContext)), result)
+    }
+}
