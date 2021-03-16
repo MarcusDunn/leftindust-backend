7050f570082926d01914cd1c07d63bcf7f2c7476
marcusdunn <marcus.s.dunn@gmail.com>
2021-03-16 16:51:23 -0700

    made ConcurrentFlowsKtTest faster

--- mockingbird/src/test/kotlin/com/leftindust/mockingbird/extensions/ConcurrentFlowsKtTest.kt
+++ mockingbird/src/test/kotlin/com/leftindust/mockingbird/extensions/ConcurrentFlowsKtTest.kt
@@ -1,6 +1,5 @@
 package com.leftindust.mockingbird.extensions
 
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -12,9 +11,9 @@ internal class ConcurrentFlowsKtTest {
         runBlocking {
             val someList = 0 until 20
 
-            val result = someList.parallelMap { delay(100); it * 2 }
+            val result = someList.parallelMap { it * 2 }
 
-            val expected = someList.map { delay(100); it * 2 }
+            val expected = someList.map { it * 2 }
 
             assertEquals(expected, result)
         }
