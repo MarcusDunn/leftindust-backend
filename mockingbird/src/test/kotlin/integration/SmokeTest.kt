df37b84b13b007851938f62c54b41b5890f9af32
marcusdunn <marcus.s.dunn@gmail.com>
2021-03-16 12:14:50 -0700

    added DB connection test (and to try webhook)

--- mockingbird/src/test/kotlin/integration/SmokeTest.kt
+++ mockingbird/src/test/kotlin/integration/SmokeTest.kt
@@ -1,9 +1,12 @@
 package integration
 
 import com.leftindust.mockingbird.MockingbirdApplication
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import javax.sql.DataSource
 
 @Tag("Integration")
 @SpringBootTest(classes = [MockingbirdApplication::class])
@@ -11,5 +14,10 @@ class SmokeTest {
     @Test
     internal fun contextLoads() {
         // checks that spring boot context loads
+    }
+
+    @Test
+    internal fun `database connected`(@Autowired dataSource: DataSource) {
+        assertEquals(true, dataSource.connection.isValid(5))
     }
 }
