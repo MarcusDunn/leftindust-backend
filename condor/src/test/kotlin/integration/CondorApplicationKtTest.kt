bb71900a09e579b9aed7677a4fb7ca1c924d9f8e
marcusdunn <marcus.s.dunn@gmail.com>
2021-03-16 12:10:21 -0700

    added test to check database connection

--- condor/src/test/kotlin/integration/CondorApplicationKtTest.kt
+++ condor/src/test/kotlin/integration/CondorApplicationKtTest.kt
@@ -1,14 +1,22 @@
 package integration
 
 import com.leftindust.condor.CondorApplication
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import javax.sql.DataSource
 
 @SpringBootTest(classes = [CondorApplication::class])
 @Tag("Integration")
 internal class CondorApplicationKtTest {
     @Test
     internal fun `context loads`() {
+    }
+
+    @Test
+    internal fun `connected to database`(@Autowired dataSource: DataSource) {
+        assertEquals(true, dataSource.connection.isValid(5))
     }
 }
