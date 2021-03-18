1fc650f1650816ccba9b3bb6e21b3176ea0a9214
marcusdunn <marcus.s.dunn@gmail.com>
2021-03-17 20:16:58 -0700

    added nullable and defaults

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/graphql/types/examples/GraphQLVisitExample.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/graphql/types/examples/GraphQLVisitExample.kt
@@ -3,11 +3,12 @@ package com.leftindust.mockingbird.graphql.types.examples
 import com.expediagroup.graphql.scalars.ID
 
 data class GraphQLVisitExample(
-    val vid: ID,
-    val timeBooked: GraphQLTimeExample,
-    val timeOfVisit: GraphQLTimeExample,
-    val title: String?,
-    val description: String?,
-    val doctor: GraphQLDoctorExample,
-    val patient: GraphQLPatientExample,
+    val vid: ID? = null,
+    val timeBooked: GraphQLTimeExample? = null,
+    val timeOfVisit: GraphQLTimeExample? = null,
+    val title: String? = null,
+    val description: String? = null,
+    val doctor: GraphQLDoctorExample? = null,
+    val patient: GraphQLPatientExample? = null,
+    val icdFoundationCode: StringFilter? = null,
 )
