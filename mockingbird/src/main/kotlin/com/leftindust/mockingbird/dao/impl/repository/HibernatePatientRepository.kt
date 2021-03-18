4d3082db25351f4c6f5fe9e79316bd7d11278c6b
marcusdunn <marcus.s.dunn@gmail.com>
2021-03-18 14:31:21 -0700

    removed unused function

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/dao/impl/repository/HibernatePatientRepository.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/dao/impl/repository/HibernatePatientRepository.kt
@@ -5,10 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.transaction.annotation.Transactional
 
 @Transactional
-interface HibernatePatientRepository : JpaRepository<Patient, Long> {
-    fun getAllByFirstNameLikeOrMiddleNameLikeOrLastNameLike(
-        firstName: String,
-        middleName: String,
-        lastName: String
-    ): List<Patient>
-}
+interface HibernatePatientRepository : JpaRepository<Patient, Long>
