ead129fa1b952ece9cc6a59bf27a5f4e7c84a796
marcusdunn <marcus.s.dunn@gmail.com>
2021-03-17 20:19:01 -0700

    removed docs

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/graphql/queries/PatientQuery.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/graphql/queries/PatientQuery.kt
@@ -1,6 +1,5 @@
 package com.leftindust.mockingbird.graphql.queries
 
-import com.expediagroup.graphql.annotations.GraphQLName
 import com.expediagroup.graphql.exceptions.GraphQLKotlinException
 import com.expediagroup.graphql.scalars.ID
 import com.expediagroup.graphql.server.operations.Query
@@ -10,17 +9,10 @@ import com.leftindust.mockingbird.dao.entity.Patient
 import com.leftindust.mockingbird.extensions.toLong
 import com.leftindust.mockingbird.graphql.types.GraphQLPatient
 import com.leftindust.mockingbird.graphql.types.examples.GraphQLPatientExample
-import com.leftindust.mockingbird.graphql.types.examples.GraphQLPersonExample
-import com.leftindust.mockingbird.graphql.types.examples.StringFilter
 import com.leftindust.mockingbird.graphql.types.input.GraphQLRangeInput
 import org.springframework.stereotype.Component
 
 @Component
-/**
- * Handles Graphql Patient Queries, all public methods in this class will appear in the schema (under query) due to
- * implementing Query.
- * @property patientDao handles integration with the database and authentication for DB requests
- */
 class PatientQuery(
     private val patientDao: PatientDao,
 ) : Query {
