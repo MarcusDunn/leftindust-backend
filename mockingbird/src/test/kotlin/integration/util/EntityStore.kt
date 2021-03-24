6168119683901cb916d64a8733b6eaba048ec075
marcusdunn <marcus.s.dunn@gmail.com>
2021-03-24 14:27:30 -0700

    added doctors endpoint

--- mockingbird/src/test/kotlin/integration/util/EntityStore.kt
+++ mockingbird/src/test/kotlin/integration/util/EntityStore.kt
@@ -1,6 +1,8 @@
 package integration.util
 
+import com.leftindust.mockingbird.dao.entity.Doctor
 import com.leftindust.mockingbird.dao.entity.Patient
+import com.leftindust.mockingbird.dao.entity.Schedule
 import com.leftindust.mockingbird.dao.entity.enums.Ethnicity
 import com.leftindust.mockingbird.dao.entity.enums.Sex
 import java.sql.Timestamp
@@ -22,5 +24,21 @@ object EntityStore {
         insuranceNumber = "marcus",
         contacts = emptySet(),
         doctors = emptySet(),
+    )
+
+    fun doctor() = Doctor(
+        firstName = "dan",
+        lastName = "shervani",
+        middleName = "the man",
+        dateOfBirth = Timestamp.valueOf("2018-01-02 09:01:15"),
+        address = "572 East 2nd Street",
+        emails = listOf("world@hello.ca"),
+        cellPhone = "1827762222",
+        workPhone = "1827772222",
+        homePhone = "1827782222",
+        title = "sir",
+        pagerNumber = "1827792222",
+        patients = emptySet(),
+        schedule = Schedule(),
     )
 }
