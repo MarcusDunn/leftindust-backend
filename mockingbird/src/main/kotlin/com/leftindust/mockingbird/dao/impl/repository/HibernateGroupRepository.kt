33b0744537a3357841d97b5845a1f974cab0e307
marcusdunn <marcus.s.dunn@gmail.com>
2021-03-24 14:49:47 -0700

    removed ref to gid

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/dao/impl/repository/HibernateGroupRepository.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/dao/impl/repository/HibernateGroupRepository.kt
@@ -3,7 +3,5 @@ package com.leftindust.mockingbird.dao.impl.repository
 import com.leftindust.mockingbird.dao.entity.MediqGroup
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface HibernateGroupRepository : JpaRepository<MediqGroup, Long> {
-    fun getByGid(gid: Long): MediqGroup?
-}
+interface HibernateGroupRepository : JpaRepository<MediqGroup, Long>
 
