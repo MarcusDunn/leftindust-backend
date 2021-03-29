f82da0748ac41344f2b81ad15d32dca191c22a6e
marcusdunn <marcus.s.dunn@gmail.com>
2021-03-28 18:47:53 -0700

    updated ContextFactory test for new gql-koltin version

--- mockingbird/src/test/kotlin/com/leftindust/mockingbird/auth/ContextFactoryTest.kt
+++ mockingbird/src/test/kotlin/com/leftindust/mockingbird/auth/ContextFactoryTest.kt
@@ -7,20 +7,25 @@ import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.http.HttpMethod
+import org.springframework.web.reactive.function.server.ServerRequest
 
 internal class ContextFactoryTest {
     private val contextFactory = ContextFactory()
 
     @Test
     fun generateContext() {
+        val mockkRequest = mockk<ServerRequest> {
+            every { method() } returns HttpMethod.POST
+            every { headers() } returns mockk() {
+                every { firstHeader("mediq-auth-token") } returns "123456"
+            }
+        }
+
         val actual = runBlocking {
             contextFactory.generateContext(
-                request = mockk {
-                    every { method } returns HttpMethod.POST
-                    every { headers["mediq-auth-token"] } returns listOf("123456")
-                }
+                request = mockkRequest
             )
         }
-        assertEquals(actual, GraphQLAuthContext(VerifiedFirebaseToken("123456")))
+        assertEquals(actual, GraphQLAuthContext(VerifiedFirebaseToken("123456"), mockkRequest))
     }
 }
