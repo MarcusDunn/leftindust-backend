c82a0eab93faf9e57df5551ec5330fc7c52fbd79
marcusdunn <marcus.s.dunn@gmail.com>
2021-03-28 18:47:19 -0700

    removed docs

--- condor/src/main/kotlin/com/leftindust/condor/CondorApplication.kt
+++ condor/src/main/kotlin/com/leftindust/condor/CondorApplication.kt
@@ -10,10 +10,6 @@ import org.springframework.boot.runApplication
 @EnableConfigurationProperties
 class CondorApplication
 
-/**
- * Entry point into the application, starts doing the spring magic
- * @param args command line arguments
- */
 fun main(args: Array<String>) {
     runApplication<CondorApplication>(*args)
 }
