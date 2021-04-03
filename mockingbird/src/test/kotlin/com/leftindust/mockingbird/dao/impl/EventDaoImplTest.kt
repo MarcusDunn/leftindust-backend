f2321e83fdb64b468ed626252be91571baf5a721
marcusdunn <marcus.s.dunn@gmail.com>
2021-04-03 16:37:11 -0700

    more tests

--- mockingbird/src/test/kotlin/com/leftindust/mockingbird/dao/impl/EventDaoImplTest.kt
+++ mockingbird/src/test/kotlin/com/leftindust/mockingbird/dao/impl/EventDaoImplTest.kt
@@ -8,6 +8,7 @@ import com.leftindust.mockingbird.dao.entity.Event
 import com.leftindust.mockingbird.dao.impl.repository.HibernateEventRepository
 import com.leftindust.mockingbird.extensions.Authorization
 import com.leftindust.mockingbird.extensions.Success
+import com.leftindust.mockingbird.graphql.types.input.GraphQLRangeInput
 import integration.util.EntityStore
 import io.mockk.coEvery
 import io.mockk.every
@@ -15,6 +16,7 @@ import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.springframework.data.domain.Pageable
 
 internal class EventDaoImplTest {
     private val hibernateEventRepository = mockk<HibernateEventRepository>()
@@ -37,5 +39,31 @@ internal class EventDaoImplTest {
         val result = runBlocking { eventDao.addEvent(event, mockk()) }
 
         assertEquals(Success(eventEntity), result)
+    }
+
+    @Test
+    fun getMany() {
+        coEvery {
+            authorizer.getAuthorization(
+                match { Action(Crud.READ to Tables.Event).isSuperset(it) },
+                any()
+            )
+        } returns Authorization.Allowed
+
+        val listOfEvent = (0 until 20).map {
+            mockk<Event>(relaxed = true) {
+                every { id } returns it.toLong()
+            }
+        }
+
+        every { hibernateEventRepository.findAll(any<Pageable>()) } returns mockk() {
+            every { toList() } returns listOfEvent
+        }
+
+        val eventDao = EventDaoImpl(hibernateEventRepository, authorizer)
+
+        val result = runBlocking { eventDao.getMany(GraphQLRangeInput(0, 20), mockk()) }
+
+        assertEquals(Success(listOfEvent), result)
     }
 }
