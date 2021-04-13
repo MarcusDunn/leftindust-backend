4778a7d82caead5a04ec3fc0968dbe16144adf8c
marcusdunn <marcus.s.dunn@gmail.com>
2021-04-13 09:58:34 -0700

    added allday and some extra annotations

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/graphql/types/input/GraphQLEventInput.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/graphql/types/input/GraphQLEventInput.kt
@@ -1,13 +1,18 @@
 package com.leftindust.mockingbird.graphql.types.input
 
+import com.expediagroup.graphql.generator.annotations.GraphQLDescription
 import com.expediagroup.graphql.generator.scalars.ID
 import com.leftindust.mockingbird.graphql.types.GraphQLTimeInput
 
 data class GraphQLEventInput(
     val title: String,
     val description: String? = null,
+    @GraphQLDescription("UTC")
     val start: GraphQLTimeInput,
+    @GraphQLDescription("UTC")
     val end: GraphQLTimeInput,
+    @GraphQLDescription("defaults to false")
+    val allDay: Boolean? = false,
     val doctors: List<ID>? = emptyList(),
     val patients: List<ID>? = emptyList(),
 )
