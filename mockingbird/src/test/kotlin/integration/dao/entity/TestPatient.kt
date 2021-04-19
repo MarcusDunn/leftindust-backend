0d68197750ddef66cd27c1e9ed57eaf6d162f626
marcusdunn <marcus.s.dunn@gmail.com>
2021-04-19 15:09:18 -0700

    avoid findAll call infavor of pages

--- mockingbird/src/test/kotlin/integration/dao/entity/TestPatient.kt
+++ mockingbird/src/test/kotlin/integration/dao/entity/TestPatient.kt
@@ -1,24 +1,20 @@
 package integration.dao.entity
 
-import com.expediagroup.graphql.generator.execution.OptionalInput
-import com.expediagroup.graphql.generator.scalars.ID
 import com.leftindust.mockingbird.MockingbirdApplication
 import com.leftindust.mockingbird.auth.MediqToken
 import com.leftindust.mockingbird.dao.PatientDao
-import com.leftindust.mockingbird.dao.entity.enums.Sex
 import com.leftindust.mockingbird.dao.impl.repository.HibernatePatientRepository
-import com.leftindust.mockingbird.graphql.types.input.GraphQLPatientInput
+import integration.util.EntityStore
 import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
-import org.hibernate.Session
-import org.hibernate.SessionFactory
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.domain.PageRequest
 import javax.transaction.Transactional
 
 @SpringBootTest(classes = [MockingbirdApplication::class])
@@ -32,12 +28,8 @@ class TestPatient(@Autowired private val patientDao: PatientDao) {
             every { uid } returns "admin"
         }
 
-        val graphQLPatientInput = GraphQLPatientInput(
-            firstName = OptionalInput.Defined("mar"),
-            lastName = OptionalInput.Defined("mar"),
-            sex = OptionalInput.Defined(Sex.Male),
-            pid = OptionalInput.Defined(ID("12"))
-        )
+        val testName = "TestPatient.test patient does not persist on invalid arguments"
+        val graphQLPatientInput = EntityStore.graphQLPatientInput(testName)
 
         runBlocking {
             assertThrows<IllegalArgumentException> {
@@ -45,6 +37,6 @@ class TestPatient(@Autowired private val patientDao: PatientDao) {
             }
         }
 
-        assertEquals(null, patientRepository.findAll().find { it.firstName == "mar" && it.lastName == "mar" })
+        assertEquals(null, patientRepository.findAll(PageRequest.of(0, 100)).find { it.middleName == testName })
     }
 }
