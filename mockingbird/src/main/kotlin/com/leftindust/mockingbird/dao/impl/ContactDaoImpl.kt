81fe24efd755e73072acd416a311cc0b00528cb1
marcusdunn <marcus.s.dunn@gmail.com>
2021-04-21 23:07:37 -0700

    probably fixed lazy init for contacts

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/dao/impl/ContactDaoImpl.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/dao/impl/ContactDaoImpl.kt
@@ -7,6 +7,7 @@ import com.leftindust.mockingbird.auth.NotAuthorizedException
 import com.leftindust.mockingbird.dao.ContactDao
 import com.leftindust.mockingbird.dao.Tables
 import com.leftindust.mockingbird.dao.entity.EmergencyContact
+import com.leftindust.mockingbird.dao.impl.repository.HibernateContactRepository
 import com.leftindust.mockingbird.dao.impl.repository.HibernatePatientRepository
 import org.apache.logging.log4j.LogManager
 import org.springframework.beans.factory.annotation.Autowired
@@ -17,17 +18,15 @@ import org.springframework.transaction.annotation.Transactional
 @Repository
 class ContactDaoImpl(
     @Autowired authorizer: Authorizer,
-    @Autowired private val patientRepository: HibernatePatientRepository,
+    @Autowired private val contactRepository: HibernateContactRepository,
 ) : AbstractHibernateDao(authorizer), ContactDao {
-
-    private val logger = LogManager.getLogger()
 
     override suspend fun getByPatient(
         pid: Long,
         requester: MediqToken
     ): Collection<EmergencyContact> {
         return if (requester can (Crud.READ to Tables.Patient)) {
-            patientRepository.getOne(pid).contacts
+            contactRepository.getAllByPatient_Id(pid)
         } else {
             throw NotAuthorizedException(requester, Crud.READ to Tables.Patient)
         }
--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/dao/impl/repository/HibernateContactRepository.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/dao/impl/repository/HibernateContactRepository.kt
@@ -1,6 +1,9 @@
 package com.leftindust.mockingbird.dao.impl.repository
 
 import com.leftindust.mockingbird.dao.entity.EmergencyContact
+import com.leftindust.mockingbird.dao.entity.Patient
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface HibernateContactRepository : JpaRepository<EmergencyContact, Long>
+interface HibernateContactRepository : JpaRepository<EmergencyContact, Long> {
+    fun getAllByPatient_Id(patient_id: Long): Collection<EmergencyContact>
+}
