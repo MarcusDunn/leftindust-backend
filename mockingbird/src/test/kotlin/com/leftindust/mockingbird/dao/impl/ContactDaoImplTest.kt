694f81c9a6107ab0026b1b273b9d21ba37423e89
marcusdunn <marcus.s.dunn@gmail.com>
2021-04-22 09:29:24 -0700

    fixed broken contructors in tests

--- mockingbird/src/test/kotlin/com/leftindust/mockingbird/dao/impl/ContactDaoImplTest.kt
+++ mockingbird/src/test/kotlin/com/leftindust/mockingbird/dao/impl/ContactDaoImplTest.kt
@@ -2,6 +2,7 @@ package com.leftindust.mockingbird.dao.impl
 
 import com.leftindust.mockingbird.auth.Authorizer
 import com.leftindust.mockingbird.dao.entity.EmergencyContact
+import com.leftindust.mockingbird.dao.impl.repository.HibernateContactRepository
 import com.leftindust.mockingbird.dao.impl.repository.HibernatePatientRepository
 import com.leftindust.mockingbird.extensions.Authorization
 import io.mockk.coEvery
@@ -14,16 +15,14 @@ import org.junit.jupiter.api.Test
 internal class ContactDaoImplTest {
 
     private val authorizer = mockk<Authorizer>()
-    private val patientRepository = mockk<HibernatePatientRepository>()
+    private val patientRepository = mockk<HibernateContactRepository>()
 
     @Test
     fun getByPatient() {
         val emergencyContact = mockk<EmergencyContact>()
 
         coEvery { authorizer.getAuthorization(any(), any()) } returns Authorization.Allowed
-        every { patientRepository.getOne(any()) } returns mockk {
-            every { contacts } returns setOf(emergencyContact)
-        }
+        every { patientRepository.getAllByPatient_Id(any()) } returns setOf(emergencyContact)
 
         val contactDaoImpl = ContactDaoImpl(authorizer, patientRepository)
         val actual = runBlocking { contactDaoImpl.getByPatient(1000L, mockk()) }
