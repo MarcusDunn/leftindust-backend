e030aee2e8e6a037ca1a73118f4fc40650cac2fc
marcusdunn <marcus.s.dunn@gmail.com>
2021-04-22 15:17:55 -0700

    madeGetByIds a single DB query

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/extensions/Repository.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/extensions/Repository.kt
@@ -1,13 +1,13 @@
 package com.leftindust.mockingbird.extensions
 
 import com.expediagroup.graphql.generator.scalars.ID
-import org.apache.logging.log4j.LogManager
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.orm.jpa.JpaObjectRetrievalFailureException
 import javax.persistence.EntityNotFoundException
 
-
-inline fun <reified T> JpaRepository<T, Long>.getByIds(ids: Collection<ID>): Set<T> =
-    ids
-        .map { this.getOne(it.toLong()) }
-        .toSet()
+inline fun <reified T> JpaRepository<T, Long>.getByIds(ids: Collection<ID>): Set<T> {
+    val result = findAllById(ids.map { it.toLong() }).toSet()
+    if (result.size != ids.size)
+        throw EntityNotFoundException("one or more of the requested ids was not found")
+    else
+        return result
+}
