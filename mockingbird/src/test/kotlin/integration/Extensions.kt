946ecb71572104aeea5b7fe27567fbc4a4980634
marcusdunn <marcus.s.dunn@gmail.com>
2021-04-23 13:34:43 -0700

    added better error reporting for integeration tests

--- mockingbird/src/test/kotlin/integration/Extensions.kt
+++ mockingbird/src/test/kotlin/integration/Extensions.kt
@@ -1,27 +1,43 @@
 package integration
 
 import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.reactive.server.returnResult
 
 fun WebTestClient.ResponseSpec.verifyOnlyDataExists(expectedQuery: String): WebTestClient.BodyContentSpec {
-    return this.expectBody()
-        .jsonPath("$DATA_JSON_PATH.$expectedQuery").exists()
-        .jsonPath(ERRORS_JSON_PATH).doesNotExist()
-        .jsonPath(EXTENSIONS_JSON_PATH).doesNotExist()
+    return try {
+        this.expectBody()
+            .jsonPath("$DATA_JSON_PATH.$expectedQuery").exists()
+            .jsonPath(ERRORS_JSON_PATH).doesNotExist()
+            .jsonPath(EXTENSIONS_JSON_PATH).doesNotExist()
+    } catch (e: AssertionError) {
+        println(this.returnResult<String>())
+        throw e
+    }
 }
 
 fun WebTestClient.ResponseSpec.verifyData(
     expectedQuery: String,
     expectedData: String
 ): WebTestClient.BodyContentSpec {
-    return this.expectStatus().isOk
-        .verifyOnlyDataExists(expectedQuery)
-        .jsonPath("$DATA_JSON_PATH.$expectedQuery").isEqualTo(expectedData)
+    return try {
+        this.expectStatus().isOk
+            .verifyOnlyDataExists(expectedQuery)
+            .jsonPath("$DATA_JSON_PATH.$expectedQuery").isEqualTo(expectedData)
+    } catch (e: AssertionError) {
+        println(this.returnResult<String>())
+        throw e
+    }
 }
 
 fun WebTestClient.ResponseSpec.verifyError(expectedError: String): WebTestClient.BodyContentSpec {
-    return this.expectStatus().isOk
-        .expectBody()
-        .jsonPath(DATA_JSON_PATH).doesNotExist()
-        .jsonPath("$ERRORS_JSON_PATH.[0].message").isEqualTo(expectedError)
-        .jsonPath(EXTENSIONS_JSON_PATH).doesNotExist()
+    return try {
+        this.expectStatus().isOk
+            .expectBody()
+            .jsonPath(DATA_JSON_PATH).doesNotExist()
+            .jsonPath("$ERRORS_JSON_PATH.[0].message").isEqualTo(expectedError)
+            .jsonPath(EXTENSIONS_JSON_PATH).doesNotExist()
+    } catch (e: AssertionError) {
+        println(this.returnResult<String>())
+        throw e
+    }
 }
