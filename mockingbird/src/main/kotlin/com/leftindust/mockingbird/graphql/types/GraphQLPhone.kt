a1bdd5ae9c135b449a3d88dc9920f284f97bb132
marcusdunn <marcus.s.dunn@gmail.com>
2021-05-16 13:39:53 -0700

    updated output type

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/graphql/types/GraphQLPhone.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/graphql/types/GraphQLPhone.kt
@@ -5,13 +5,8 @@ import com.leftindust.mockingbird.dao.entity.Phone
 
 @GraphQLName("Phone")
 data class GraphQLPhone(
-    val number: Long,
+    val number: String,
     val type: GraphQLPhoneType
 ) {
-    constructor(stringyPhoneNumber: String, type: GraphQLPhoneType) : this(
-        number = stringyPhoneNumber.replace(Regex("[^0-9]"), "").toLong(),
-        type = type
-    )
-
     constructor(phone: Phone) : this(phone.number, phone.type)
 }
