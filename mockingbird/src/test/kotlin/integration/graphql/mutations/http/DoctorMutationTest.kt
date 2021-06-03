e3ee6afec2127f08d2e740e2c98b029a1b021c45
marcusdunn <marcus.s.dunn@gmail.com>
2021-06-02 17:31:44 -0700

    updated doc mutation test to not be transactional

--- mockingbird/src/test/kotlin/integration/graphql/mutations/http/DoctorMutationTest.kt
+++ mockingbird/src/test/kotlin/integration/graphql/mutations/http/DoctorMutationTest.kt
@@ -4,6 +4,8 @@ import com.leftindust.mockingbird.MockingbirdApplication
 import com.leftindust.mockingbird.auth.Authorizer
 import com.leftindust.mockingbird.auth.ContextFactory
 import com.leftindust.mockingbird.auth.MediqToken
+import com.leftindust.mockingbird.dao.entity.Doctor
+import com.leftindust.mockingbird.dao.entity.MediqUser
 import com.leftindust.mockingbird.dao.impl.repository.HibernateDoctorRepository
 import com.leftindust.mockingbird.dao.impl.repository.HibernateUserRepository
 import com.leftindust.mockingbird.extensions.Authorization
@@ -13,9 +15,14 @@ import integration.APPLICATION_JSON_MEDIA_TYPE
 import integration.GRAPHQL_ENDPOINT
 import integration.GRAPHQL_MEDIA_TYPE
 import integration.verifyOnlyDataExists
+import io.ktor.http.*
 import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
+import org.hibernate.SessionFactory
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -23,22 +30,31 @@ import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWeb
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.domain.PageRequest
 import org.springframework.test.web.reactive.server.WebTestClient
-import org.springframework.transaction.annotation.Transactional
 
 @SpringBootTest(classes = [MockingbirdApplication::class])
 @AutoConfigureWebTestClient
 @Tag("Integration")
-@Transactional
 class DoctorMutationTest(
     @Autowired private val testClient: WebTestClient,
     @Autowired private val doctorRepository: HibernateDoctorRepository,
     @Autowired private val userRepository: HibernateUserRepository,
+    @Autowired private val sessionFactory: SessionFactory,
 ) {
+    private val doctorCount = doctorRepository.count()
+    private val userCount = userRepository.count()
+
     @MockkBean
     private lateinit var contextFactory: ContextFactory
 
     @MockkBean
     private lateinit var authorizer: Authorizer
+
+    @BeforeEach
+    @AfterEach
+    internal fun setUp() {
+        assertEquals(doctorCount, doctorRepository.count()) { "Leaked doctors in DoctorMutationTest ${doctorRepository.findAll().map { it.nameInfo }}" }
+        assertEquals(userCount, userRepository.count()) { "Leaked users in DoctorMutationTest ${userRepository.findAll().map { it.nameInfo }}" }
+    }
 
     @Test
     internal fun `create doctor with user`() {
@@ -64,10 +80,11 @@ class DoctorMutationTest(
             .verifyOnlyDataExists("addDoctor")
 
         val result =
-            doctorRepository.findAll().find { it.nameInfo.firstName == "doc" && it.nameInfo.lastName == "james" }
-        val userResult = userRepository.findAll().find { it.uniqueId == "new uid" }
-        assertNotNull(result)
-        assertNotNull(userResult)
+            doctorRepository.findAll().find { it.nameInfo.firstName == "doc" && it.nameInfo.lastName == "james" }!!
+        val userResult = userRepository.findAll().find { it.uniqueId == "new uid" }!!
+
+        doctorRepository.deleteById(result.id!!)
+        userRepository.deleteById(userResult.id!!)
     }
 
     @Test
@@ -78,15 +95,14 @@ class DoctorMutationTest(
         }
         coEvery { authorizer.getAuthorization(any(), mediqToken) } returns Authorization.Allowed
 
-        val mutation = "addDoctor"
-
         testClient.post()
             .uri(GRAPHQL_ENDPOINT)
             .accept(APPLICATION_JSON_MEDIA_TYPE)
             .contentType(GRAPHQL_MEDIA_TYPE)
             .bodyValue(
-                """mutation { $mutation(doctor: {
-                |    nameInfo: {firstName: "doc", lastName: "james"},
+                //language=GraphQL
+                """mutation { addDoctor(doctor: {
+                |    nameInfo: {firstName: "lebron", lastName: "james"},
                 |    dateOfBirth: {day: 23, month: Jan, year: 1999}, 
                 |    addresses: [{addressType: Home, address: "182 yeet st", city: "Vancouver", country: Canada, province: "BritishColumbia", postalCode: "h7g1p1"}]
                 |})
@@ -97,12 +113,53 @@ class DoctorMutationTest(
                 |""".trimMargin()
             )
             .exchange()
-            .verifyOnlyDataExists(mutation)
+            .verifyOnlyDataExists("addDoctor")
+            .jsonPath("data.addDoctor.firstName")
+            .isEqualTo("lebron")
 
         val result = doctorRepository.findAll(PageRequest.of(0, 10))
             .iterator()
             .asSequence()
-            .find { it.address.firstOrNull()?.address == "182 yeet st" }
-        assertNotNull(result)
+            .find { it.address.firstOrNull()?.address == "182 yeet st" }!!
+
+        doctorRepository.deleteById(result.id!!)
     }
+//
+//    @Test
+//    internal fun `edit doctor`() {
+//        val mediqToken = mockk<MediqToken>()
+//        coEvery { contextFactory.generateContext(any()) } returns mockk(relaxed = true) {
+//            every { mediqAuthToken } returns mediqToken
+//        }
+//        coEvery { authorizer.getAuthorization(any(), mediqToken) } returns Authorization.Allowed
+//
+//       val doctor =  doctorRepository.save(EntityStore.doctor("DoctorMutationTest.`edit doctor`"))
+//
+//        testClient.post()
+//            .uri(GRAPHQL_ENDPOINT)
+//            .accept(APPLICATION_JSON_MEDIA_TYPE)
+//            .contentType(GRAPHQL_MEDIA_TYPE)
+//            .bodyValue(
+//                //language=GraphQL
+//                """mutation { updateDoctor(doctor: {did: {id: }})
+//                | {
+//                | firstName
+//                | }
+//                |}
+//                |""".trimMargin()
+//            )
+//            .exchange()
+//            .verifyOnlyDataExists("addDoctor")
+//            .jsonPath("data.addDoctor.firstName")
+//            .isEqualTo("doc")
+//
+//        val session = sessionFactory.openSession()
+//        try {
+//            session.delete(session.get(Doctor::class.java, doctor.id!!))
+//        }catch (e: Exception) {
+//            throw e
+//        } finally {
+//            session.close()
+//        }
+//    }
 }
