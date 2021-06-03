b784f9baaf5f70f30901c44655a48471e88bd395
marcusdunn <marcus.s.dunn@gmail.com>
2021-06-02 20:31:21 -0700

    added whitespace and case-agnostic stringfilters

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/graphql/types/example/GraphQLPatientExample.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/graphql/types/example/GraphQLPatientExample.kt
@@ -7,15 +7,13 @@ import com.leftindust.mockingbird.dao.entity.Patient
 import com.leftindust.mockingbird.dao.entity.Patient_
 import com.leftindust.mockingbird.graphql.types.input.GraphQLDateInput
 import java.sql.Date
-import javax.persistence.criteria.CriteriaBuilder
-import javax.persistence.criteria.From
-import javax.persistence.criteria.Predicate
-import javax.persistence.criteria.Root
+import javax.persistence.criteria.*
 import javax.persistence.metamodel.SingularAttribute
 
 @GraphQLIgnore
 interface PredicateInput {
     val strict: Boolean
+
     @GraphQLIgnore
     fun combineWithStrict(criteriaBuilder: CriteriaBuilder, vararg predicates: Predicate): Predicate {
         return if (strict) {
@@ -44,12 +42,12 @@ interface Filter<G> : PredicateInput {
 
 @GraphQLName("PatientExample")
 data class GraphQLPatientExample(
-    val firstName: StringFilter? = null,
-    val lastName: StringFilter? = null,
+    val firstName: CaseAgnosticStringFilter? = null,
+    val lastName: CaseAgnosticStringFilter? = null,
     val dateOfBirth: DateFilter? = null,
-    val insuranceNumber: StringFilter? = null,
+    val insuranceNumber: WhiteSpaceAgnosticStringFilter? = null,
     override val strict: Boolean,
-): Example<Patient> {
+) : Example<Patient> {
     override fun toPredicate(criteriaBuilder: CriteriaBuilder, root: Root<Patient>): Predicate {
         val patientNameInfoJoin = root.join(Patient_.nameInfo)
         val predicates = listOfNotNull(
@@ -66,7 +64,7 @@ data class DateFilter(
     val before: GraphQLDateInput? = null,
     val after: GraphQLDateInput? = null,
     override val strict: Boolean,
-): Filter<Date> {
+) : Filter<Date> {
 
     override fun <Z, X> toPredicate(
         criteriaBuilder: CriteriaBuilder,
@@ -81,7 +79,116 @@ data class DateFilter(
     }
 }
 
-data class StringFilter(
+data class FloatFilter(
+    val gt: Int? = null,
+    val lt: Int? = null,
+    override val strict: Boolean,
+) : Filter<Int> {
+    override fun <Z, X> toPredicate(
+        criteriaBuilder: CriteriaBuilder,
+        root: From<Z, X>,
+        columnName: SingularAttribute<X, Int>
+    ): Predicate {
+        val predicates = listOfNotNull(
+            gt?.let { criteriaBuilder.greaterThanOrEqualTo(root.get(columnName), gt) },
+            lt?.let { criteriaBuilder.lessThanOrEqualTo(root.get(columnName), lt) },
+        ).toTypedArray()
+        return combineWithStrict(criteriaBuilder, *predicates)
+    }
+}
+
+data class IntFilter(
+    val gt: Int? = null,
+    val lt: Int? = null,
+    val eq: Int? = null,
+    val ne: Int? = null,
+    override val strict: Boolean
+) : Filter<Int> {
+    override fun <Z, X> toPredicate(
+        criteriaBuilder: CriteriaBuilder,
+        root: From<Z, X>,
+        columnName: SingularAttribute<X, Int>
+    ): Predicate {
+        val predicates = listOfNotNull(
+            gt?.let { criteriaBuilder.greaterThanOrEqualTo(root.get(columnName), gt) },
+            lt?.let { criteriaBuilder.lessThanOrEqualTo(root.get(columnName), lt) },
+            eq?.let { criteriaBuilder.equal(root.get(columnName), eq) },
+            ne?.let { criteriaBuilder.notEqual(root.get(columnName), ne) },
+        ).toTypedArray()
+        return combineWithStrict(criteriaBuilder, *predicates)
+    }
+}
+
+
+class WhiteSpaceAgnosticStringFilter(
+    eq: String? = null,
+    ne: String? = null,
+    contains: String? = null,
+    notContain: String? = null,
+    startsWith: String? = null,
+    notStartWith: String? = null,
+    endsWith: String? = null,
+    notEndWith: String? = null,
+    strict: Boolean,
+) : AbstractStringFilter(
+    eq = eq?.replace(" ", ""),
+    ne = ne?.replace(" ", ""),
+    contains = contains?.replace(" ", ""),
+    notContain = notContain?.replace(" ", ""),
+    startsWith = startsWith?.replace(" ", ""),
+    notStartWith = notStartWith?.replace(" ", ""),
+    endsWith = endsWith?.replace(" ", ""),
+    notEndWith = notEndWith?.replace(" ", ""),
+    strict = strict,
+) {
+
+    override fun <X, Z> editColumn(
+        criteriaBuilder: CriteriaBuilder,
+        root: From<Z, X>,
+        columnName: SingularAttribute<X, String>
+    ): Expression<String> {
+        return criteriaBuilder.function(
+            "REPLACE",
+            String::class.java,
+            root.get(columnName),
+            criteriaBuilder.literal(" "),
+            criteriaBuilder.literal("")
+        )
+    }
+}
+
+class CaseAgnosticStringFilter(
+    eq: String? = null,
+    ne: String? = null,
+    contains: String? = null,
+    notContain: String? = null,
+    startsWith: String? = null,
+    notStartWith: String? = null,
+    endsWith: String? = null,
+    notEndWith: String? = null,
+    strict: Boolean,
+) : AbstractStringFilter(
+    eq = eq?.uppercase(),
+    ne = ne?.uppercase(),
+    contains = contains?.uppercase(),
+    notContain = notContain?.uppercase(),
+    startsWith = startsWith?.uppercase(),
+    notStartWith = notStartWith?.uppercase(),
+    endsWith = endsWith?.uppercase(),
+    notEndWith = notEndWith?.uppercase(),
+    strict = strict,
+) {
+
+    override fun <X, Z> editColumn(
+        criteriaBuilder: CriteriaBuilder,
+        root: From<Z, X>,
+        columnName: SingularAttribute<X, String>
+    ): Expression<String> {
+        return criteriaBuilder.upper(root.get(columnName))
+    }
+}
+
+abstract class AbstractStringFilter(
     val eq: String? = null,
     val ne: String? = null,
     val contains: String? = null,
@@ -98,17 +205,23 @@ data class StringFilter(
         root: From<Z, X>,
         columnName: SingularAttribute<X, String>
     ): Predicate {
-        val column = criteriaBuilder.upper(root.get(columnName))
+        val column = editColumn(criteriaBuilder, root, columnName)
         val predicates = listOfNotNull(
-            eq?.let { criteriaBuilder.equal(column, eq.uppercase()) },
-            ne?.let { criteriaBuilder.notEqual(column, ne.uppercase()) },
-            contains?.let { criteriaBuilder.like(column, "%$contains%".uppercase()) },
-            notContain?.let { criteriaBuilder.notLike(column, "%$notContain%".uppercase()) },
-            startsWith?.let { criteriaBuilder.like(column, "$startsWith%".uppercase()) },
-            notStartWith?.let { criteriaBuilder.notLike(column, "$notStartWith%".uppercase()) },
-            endsWith?.let { criteriaBuilder.like(column, "%$endsWith".uppercase()) },
-            notEndWith?.let { criteriaBuilder.notLike(column, "%$notEndWith".uppercase()) },
+            eq?.let { criteriaBuilder.equal(column, eq) },
+            ne?.let { criteriaBuilder.notEqual(column, ne) },
+            contains?.let { criteriaBuilder.like(column, "%$contains%") },
+            notContain?.let { criteriaBuilder.notLike(column, "%$notContain%") },
+            startsWith?.let { criteriaBuilder.like(column, "$startsWith%") },
+            notStartWith?.let { criteriaBuilder.notLike(column, "$notStartWith%") },
+            endsWith?.let { criteriaBuilder.like(column, "%$endsWith") },
+            notEndWith?.let { criteriaBuilder.notLike(column, "%$notEndWith") },
         ).toTypedArray()
         return combineWithStrict(criteriaBuilder, *predicates)
     }
+
+    abstract fun <X, Z> editColumn(
+        criteriaBuilder: CriteriaBuilder,
+        root: From<Z, X>,
+        columnName: SingularAttribute<X, String>
+    ): Expression<String>
 }
