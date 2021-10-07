2cd0065f3c00698a441617508ed838c9157e411e
MarcusDunn <marcus.s.dunn@gmail.com>
2021-10-07 14:21:07 -0700

    removed file support for formTemplates and added changes to DB

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/dao/entity/MediqRecord.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/dao/entity/MediqRecord.kt
@@ -20,14 +20,11 @@ class MediqRecord(
     @Column(name = "json_blob", length = 10_000, nullable = false)
     @Basic(fetch = FetchType.LAZY)
     val jsonBlob: String,
-    @OneToMany
-    val attachments: Set<Attachment>
 ) : AbstractJpaPersistable() {
     constructor(record: GraphQLRecordInput, patient: Patient) : this(
         patient = patient,
         creationDate = Timestamp.from(Instant.now()),
         type = record.recordType,
         jsonBlob = record.jsonBlob,
-        attachments = emptySet(), // TODO allow explicit attachments in [GraphQLRecordInput]
     )
 }
--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/dao/entity/enums/FileDataType.kt
+++
@@ -1,6 +0,0 @@
-package com.leftindust.mockingbird.dao.entity.enums
-
-enum class FileDataType {
-    Image,
-    Pdf,
-}
