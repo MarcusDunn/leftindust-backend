32ffbfc66e0ed6c4faacc9f19fe2e6c9853214f0
MarcusDunn <marcus.s.dunn@gmail.com>
2022-01-23 21:47:27 -0800

    weee woooo

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/dao/entity/DoctorPatient.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/dao/entity/DoctorPatient.kt
@@ -6,12 +6,9 @@ import java.time.Instant
 import javax.persistence.Entity
 import javax.persistence.FetchType
 import javax.persistence.ManyToOne
-import javax.persistence.Table
-import javax.persistence.UniqueConstraint
 
 // note that for the sake of persisting updates we treat the doctor as owning this table.
 @Entity
-@Table(uniqueConstraints = [UniqueConstraint(columnNames = ["patient", "doctor"])])
 class DoctorPatient(
     @ManyToOne(fetch = FetchType.LAZY)
     var patient: Patient,
