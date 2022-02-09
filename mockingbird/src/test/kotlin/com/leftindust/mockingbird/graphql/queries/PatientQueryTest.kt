a3ffafdb4c65c346a99b2af36815651f9462a5e0
Marcus Dunn <marcus.s.dunn@gmail.com>
2022-02-09 15:53:16 -0800

    PatientQuery now has different endpoints for different ops for easier fuzzing.

--- mockingbird/src/test/kotlin/com/leftindust/mockingbird/graphql/queries/PatientQueryTest.kt
+++ mockingbird/src/test/kotlin/com/leftindust/mockingbird/graphql/queries/PatientQueryTest.kt
@@ -3,9 +3,10 @@ package com.leftindust.mockingbird.graphql.queries
 import com.leftindust.mockingbird.auth.GraphQLAuthContext
 import com.leftindust.mockingbird.dao.PatientDao
 import com.leftindust.mockingbird.dao.entity.Patient
-import com.leftindust.mockingbird.extensions.gqlID
 import com.leftindust.mockingbird.graphql.types.GraphQLPatient
 import com.leftindust.mockingbird.graphql.types.input.GraphQLRangeInput
+import com.leftindust.mockingbird.graphql.types.search.example.GraphQLPatientExample
+import com.leftindust.mockingbird.graphql.types.search.filter.CaseAgnosticStringFilter
 import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
@@ -25,11 +26,14 @@ internal class PatientQueryTest {
         val mockkPatient = mockk<Patient>(relaxed = true) {
             every { id } returns patientID
         }
-        coEvery { patientDao.getPatientsByPids(listOf(GraphQLPatient.ID(patientID)), any()) } returns listOf(mockkPatient)
+        coEvery { patientDao.getPatientsByPids(listOf(GraphQLPatient.ID(patientID)), any()) } returns listOf(
+            mockkPatient
+        )
         every { authContext.mediqAuthToken } returns mockk()
         val graphQLPatient = GraphQLPatient(mockkPatient, authContext)
         val patientQuery = PatientQuery(patientDao)
-        val result = runBlocking { patientQuery.patients(pids = listOf(GraphQLPatient.ID(patientID)), authContext = authContext) }
+        val result =
+            runBlocking { patientQuery.patientsByPid(listOf(GraphQLPatient.ID(patientID)), authContext = authContext) }
         assertEquals(listOf(graphQLPatient), result)
     }
 
@@ -50,7 +54,36 @@ internal class PatientQueryTest {
         )
         val patientQuery = PatientQuery(patientDao)
         val graphQLPatient = GraphQLPatient(mockkPatient, authContext)
-        val result = runBlocking { patientQuery.patients(GraphQLRangeInput(0, 5), authContext = authContext) }
+        val result = runBlocking { patientQuery.patientsByRange(GraphQLRangeInput(0, 5), authContext = authContext) }
+        assertEquals((0 until 5).map { graphQLPatient }, result)
+    }
+
+    @Test
+    fun patientsByExample() {
+        val patientID = UUID.randomUUID()
+
+        val mockkPatient = mockk<Patient>(relaxed = true) {
+            every { id } returns patientID
+        }
+        every { authContext.mediqAuthToken } returns mockk()
+        coEvery { patientDao.searchByExample(any(), any()) } returns listOf(
+            mockkPatient,
+            mockkPatient,
+            mockkPatient,
+            mockkPatient,
+            mockkPatient
+        )
+        val patientQuery = PatientQuery(patientDao)
+        val graphQLPatient = GraphQLPatient(mockkPatient, authContext)
+        val result = runBlocking {
+            patientQuery.patientsByExample(
+                GraphQLPatientExample(
+                    firstName = CaseAgnosticStringFilter(eq = "Marcus", strict = true),
+                    strict = true
+                ),
+                authContext = authContext
+            )
+        }
         assertEquals((0 until 5).map { graphQLPatient }, result)
     }
 }
