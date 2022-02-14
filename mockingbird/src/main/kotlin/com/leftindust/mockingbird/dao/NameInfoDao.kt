ec93667f7abe3bd8c073d674de03421dff9fac0f
MarcusDunn <marcus.s.dunn@gmail.com>
2022-02-13 19:24:46 -0800

    added tests for NameInfoDaoImpl and NameInfoDao

--- mockingbird/src/main/kotlin/com/leftindust/mockingbird/dao/NameInfoDao.kt
+++ mockingbird/src/main/kotlin/com/leftindust/mockingbird/dao/NameInfoDao.kt
@@ -2,8 +2,10 @@ package com.leftindust.mockingbird.dao
 
 import com.leftindust.mockingbird.auth.MediqToken
 import com.leftindust.mockingbird.dao.entity.NameInfo
+import javax.persistence.EntityNotFoundException
 
 interface NameInfoDao {
-    suspend fun getByUniqueId(uid: String, requester: MediqToken): NameInfo
     suspend fun findByUniqueId(uid: String, requester: MediqToken): NameInfo?
+    suspend fun getByUniqueId(uid: String, requester: MediqToken): NameInfo =
+        findByUniqueId(uid, requester) ?: throw EntityNotFoundException("Could not find name with uid $uid")
 }
